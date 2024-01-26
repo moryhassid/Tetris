7e1c41a79e0d305aa9496e91e652da39309f6be2
Mory_Hassid <mory9870@gmail.com>
2024-01-26 11:49:44 +0200

    1. Moving the piece of puzzle on the horizontal axis 2. Adding borders to the games so the piece of puzzle will not offstage.

--- main.py
+++ main.py
@@ -11,13 +11,14 @@ YELLOW = (255, 255, 0)
 RED = (255, 0, 0)
 ORANGE = (255, 140, 0)
 PURPLE = (128, 0, 128)
-WIDTH_SCREEN = 300
+WIDTH_SCREEN = 320
 HEIGHT_SCREEN = 550
 BACKGROUND_COLOR = (255, 255, 255)
 NUM_TYPES_OF_PUZZLE_PIECES = 8
 SQUARE_WIDTH = 20
 SQUARE_HEIGHT = 20
 VISIBLE_BRICK = 1
+STEP_SIZE_TO_MOVE_PUZZLE_PIECE_ON_X_AXIS = 10
 
 
 def get_piece_of_puzzle():
@@ -99,6 +100,25 @@ class PuzzlePiece:
         pass
 
 
+def have_reached_bottom_of_screen(puzzle, pos_y):
+    lowest_part_of_the_puzzle_y_axis = pos_y + puzzle.shape[0] * SQUARE_HEIGHT
+
+    if lowest_part_of_the_puzzle_y_axis >= HEIGHT_SCREEN:
+        return True
+
+    return False
+
+
+def have_reached_left_or_right_borders(puzzle, pos_x):
+    right_part_of_the_puzzle_x_axis = pos_x + puzzle.shape[1] * SQUARE_WIDTH
+    left_part_of_the_puzzle_x_axis = pos_x
+
+    if right_part_of_the_puzzle_x_axis >= WIDTH_SCREEN or left_part_of_the_puzzle_x_axis <= 0:
+        return True
+
+    return False
+
+
 class Tetromino:
     def __init__(self, x, y, shape):
         colors = [BLUE, LIGHT_BLUE, GREEN, YELLOW, RED, ORANGE, PURPLE]
@@ -124,10 +144,14 @@ if __name__ == '__main__':
     puzzle_piece = get_piece_of_puzzle()
     color_of_piece = random.choice([BLUE, LIGHT_BLUE, GREEN, YELLOW, RED, ORANGE, PURPLE])
     counter_key_pressed = 0
+    counter_to_move_down_puzzle_piece = 0
     puzzle_piece_x = WIDTH_SCREEN // 2
     puzzle_piece_y = HEIGHT_SCREEN // 10
+    has_reached_floor = False
+    has_reached_left_right_borders = False
     while True:
         screen.fill(BACKGROUND_COLOR)
+        counter_to_move_down_puzzle_piece += 1
         draw_piece_of_puzzle(piece_of_puzzle=puzzle_piece,
                              given_screen=screen,
                              color_chosen=color_of_piece, start_x_pos=puzzle_piece_x, start_y_pos=puzzle_piece_y)
@@ -138,26 +162,39 @@ if __name__ == '__main__':
                 print('Mory is closing the game')
                 exit(0)
 
+        if counter_to_move_down_puzzle_piece > 10 and not has_reached_floor:
+            puzzle_piece_y += 20
+            counter_to_move_down_puzzle_piece = 0
+
         keys = pygame.key.get_pressed()
 
         if keys[pygame.K_RIGHT]:
             print('Right was pressed')
-            puzzle_piece_x += 20
+            if not has_reached_left_right_borders:
+                puzzle_piece_x += STEP_SIZE_TO_MOVE_PUZZLE_PIECE_ON_X_AXIS
         elif keys[pygame.K_LEFT]:
             print('Left was pressed')
-            puzzle_piece_x -= 20
+            if not has_reached_left_right_borders:
+                puzzle_piece_x -= STEP_SIZE_TO_MOVE_PUZZLE_PIECE_ON_X_AXIS
         elif keys[pygame.K_UP]:
             counter_key_pressed += 1
             if counter_key_pressed == 2:
                 puzzle_piece = np.rot90(puzzle_piece)
                 counter_key_pressed = 0
             print('Up was pressed')
-
         elif keys[pygame.K_DOWN]:
             print('Down was pressed')
+            if not has_reached_floor:
+                puzzle_piece_y += 5
         elif keys[pygame.K_ESCAPE]:
             print('Mory is closing the game, he has pressed Escape button')
             exit(0)
+
+        has_reached_floor = have_reached_bottom_of_screen(puzzle=puzzle_piece,
+                                                          pos_y=puzzle_piece_y)
+
+        has_reached_left_right_borders = have_reached_left_or_right_borders(puzzle=puzzle_piece,
+                                                                            pos_x=puzzle_piece_x)
 
         # Very important each frame we should use to  put your work on screen
         pygame.display.flip()
